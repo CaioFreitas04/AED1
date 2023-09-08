f885ed11ccc3d85b0845d2bd74569b6947cb279d
CaioFreitas04 <127106101+CaioFreitas04@users.noreply.github.com>
2023-09-08 08:51:48 -0300

    Add files via upload

--- L6/EX_A/Lista.h
+++ L6/EX_A/Lista.h
@@ -1,18 +1,17 @@
 #ifndef LISTA_H
 #define LISTA_H
 
-#define MAX 100
 typedef struct aluno alu;
 typedef struct lista list;
 
-list *criar();
+list *criar(int size);
 void limpar(list *l);
 int inserirIni(list *l, alu input);
 int inserirFim(list *l, alu input);
 int inserirPos(list *l, alu input, int pos);
 int removerIni(list *l);
 int removerFim(list *l);
-int removerPosi(list *l, int pos);
+int removerPos(list *l, int pos);
 int removerItem(list *l, alu input);
 int buscChave(list *l, int key, alu *out);
 int buscPos(list *l, int pos, alu *out);
@@ -20,5 +19,8 @@ int listVazia(list *l);
 int listCheia(list *l);
 int tamanho(list *l);
 void mostrar(list *l);
+int contemItem(list *l, alu input);
+int invertList(list *l, list *invert);
+int itemCont(list *l, alu input);
 
 #endif
